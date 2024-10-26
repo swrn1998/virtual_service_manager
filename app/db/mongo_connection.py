14c18815f82e8d121a5a8c5ec9abad342a068551
Nikhil Soni <35272171+swrn1998@users.noreply.github.com>
2024-10-26 20:13:38 +0530

    Update mongo_connection.py

--- app/db/mongo_connection.py
+++ app/db/mongo_connection.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from app.config import Config
 
 # Set up the MongoDB connection
-client = MongoClient(Config.MONGO_URI)
+client = MongoClient(Config.MONGO_URI, tls=True, tlsAllowInvalidCertificates=False)
 db = client[Config.DATABASE_NAME]
 collection = db['api_requests']
 
