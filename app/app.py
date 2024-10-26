6f613ce386bfddbe98aaaf9ea6bbe343887fefa0
Nikhil Soni <35272171+swrn1998@users.noreply.github.com>
2024-10-26 20:00:11 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes.api_routes import api_blueprint
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ app = Flask(__name__)
 app.register_blueprint(api_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to the specified PORT environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
